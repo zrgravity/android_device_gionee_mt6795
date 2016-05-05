cb51ec0691a572fd1e6f7120e0bfe1b91497545b
root-expert <20chris09@gmail.com>
2016-05-05 05:36:29 -0400

    Add Torch, fix typo, add sensors.
    Change-Id: Ifd69ee5a947a1b252574262d3d7b07df61b29c89

--- device_BLU_PURE_XL.mk
+++ device_BLU_PURE_XL.mk
@@ -45,7 +45,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     	$(LOCAL_PATH)/configs/media_codecs.xml:system/etc/permissions/media_codecs.xml \
-	$(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
 	$(LOCAL_PATH)/configs/audio_device.xml:system/etc/audio_device.xml \
     	$(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
@@ -53,6 +52,10 @@ PRODUCT_COPY_FILES += \
 #Bluetooth
 PRODUCT_PACKAGES += \
     	libbt-vendor
+
+#Torch
+PRODUCT_PACKAGES += \
+	Torch
 	
 #Ramdisk
 PRODUCT_COPY_FILES += \
@@ -81,7 +84,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	$(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/andorid.hardware.camera.xml \
+	$(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
 	$(LOCAL_PATH)/configs/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
@@ -90,6 +93,13 @@ PRODUCT_COPY_FILES += \
     	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
     	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+   	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
     	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
