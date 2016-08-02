826cc463956b735701e9d2a060c89f966e24ad5a
Bartosz Bem (bemolx) <kalkobb99@gmail.com>
2016-08-02 12:00:12 +0200

    PRODUCT_MODEL = Pure XL (P0010UU) [1/3]

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := MT6795
+PRODUCT_RELEASE_NAME := Pure XL
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -10,6 +10,6 @@ $(call inherit-product, device/gionee/mt6795/device_mt6795.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mt6795
 PRODUCT_NAME := cm_mt6795
-PRODUCT_BRAND := Gionee
-PRODUCT_MODEL := MT6795
+PRODUCT_BRAND := BLU
+PRODUCT_MODEL := Pure XL (P0010UU)
 PRODUCT_MANUFACTURER := Gionee
